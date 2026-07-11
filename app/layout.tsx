4ff99891e995791e94bf30da0d07bc980c8746a5
Xtarii <carlalvinelias@gmail.com>
2026-07-11 13:12:40 +0200

    using MUI with tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import DBContext from "@/components/db/db";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,11 +26,13 @@ export default function RootLayout({ children, }: Readonly<{children: React.Reac
           className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="w-full h-full">
-                <DBContext url={process.env.BACKEND}>
-                    <div className="flex m-0 w-full h-full">
-                        {children}
-                    </div>
-                </DBContext>
+                <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+                    <DBContext url={process.env.BACKEND}>
+                        <div className="flex m-0 w-full h-full">
+                            {children}
+                        </div>
+                    </DBContext>
+                </AppRouterCacheProvider>
             </body>
         </html>
     );
