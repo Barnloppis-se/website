32e19c7bbb21dfe39bfeb3a6bd5090634184e77c
Xtarii <carlalvinelias@gmail.com>
2026-07-14 12:18:29 +0200

    moved providers and theme into application instead of layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import DBContext from "@/components/db/db";
+import Application from "../components/application";
 import "./globals.css";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -26,13 +25,11 @@ export default function RootLayout({ children, }: Readonly<{children: React.Reac
           className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
         >
             <body className="w-full h-full">
-                <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-                    <DBContext url={process.env.NEXT_PUBLIC_BACKEND}>
-                        <div className="flex m-0 w-full h-full">
-                            {children}
-                        </div>
-                    </DBContext>
-                </AppRouterCacheProvider>
+                <Application>
+                    <div className="flex m-0 w-full h-full">
+                        {children}
+                    </div>
+                </Application>
             </body>
         </html>
     );
---
+++ components/application.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { colors } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import { ReactNode } from "react";
+import DBContext from "./db/db";
+
+/**
+ * Application theme
+ */
+export const theme = createTheme({
+    palette: {
+        background: {
+            default: colors.common.white,
+            paper: colors.blue[50]
+        },
+    }
+});
+
+
+
+/**
+ * Application base component
+ */
+export default function Application({ children } : { children: ReactNode }): ReactNode {
+    const theme = useTheme();
+
+    return(
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+                <DBContext url={process.env.NEXT_PUBLIC_BACKEND}>
+                    {children}
+                </DBContext>
+            </ThemeProvider>
+        </AppRouterCacheProvider>
+    );
+}
