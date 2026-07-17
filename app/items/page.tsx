401dca004eb3e7f1230e79f79cf5f640a66e4d2d
Xtarii <carlalvinelias@gmail.com>
2026-07-17 14:50:10 +0200

    item display controller added

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,41 +1,18 @@
 "use client"
-import Dropdown from "@/components/ui/Dropdown";
 import ItemDisplay from "@/components/ui/items/ItemDisplay";
-import NumberField from "@/components/ui/NumberField";
 import { getItems } from "@barnloppis-se/api";
 import { ItemObject } from "@barnloppis-se/types/dist/src/data/item";
 import { DBSearch } from "@barnloppis-se/types/dist/src/upload/uploadData";
-import { Box, Button, Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 
 /**
  * Default values
  */
 const defaults = {
-    /**
-     * The amount of items to
-     * show by default.
-     */
     show: 50,
-
-    /**
-     * Default sorting method
-     */
-    sort: {
-        /**
-         * Method
-         */
-        method: "date",
-        /**
-         * Label
-         */
-        label: "Nyast",
-
-        /**
-         * Seller
-         */
-        seller: 1
-    }
+    sort: "date" as DBSearch,
+    seller: "*" as number | "*"
 }
 
 
@@ -45,18 +22,18 @@ const defaults = {
  */
 export default function Items() {
     const [ items, setItems ] = useState<ItemObject[]>([]);
-    const [ max, setMax ] = useState(50);
-    const [ show, setShow ] = useState(defaults.show);
-    const [ first, setFirst ] = useState(0);
-    const [ sort, setSort ] = useState<DBSearch>(defaults.sort.method as DBSearch);
-    const [ seller, setSeller ] = useState<{ value: number, use: number | "*" }>({value: defaults.sort.seller, use: defaults.sort.seller});
 
-    const apply = async () => {
+    const [ max, setMax ] = useState(50);
+    const [ first, setFirst ] = useState(0);
+
+
+
+    const apply = async (state: typeof defaults) => {
         // Start load
 
-        const res = await getItems("*", first, show, sort, seller.use);
+        const res = await getItems("*", first, state.show, state.sort, state.seller);
         if(res.status === true) {
-            setItems(res.data.items.slice(first, first + show));
+            setItems(res.data.items.slice(first, first + state.show));
             setMax(res.data.size);
 
         } else { console.log(res); }
@@ -64,61 +41,13 @@ export default function Items() {
         // End load
     }
 
-    useEffect(() => { apply() }, []);
+    useEffect(() => { apply(defaults) }, []);
 
     return(
         <Box className="m-12">
-            <Box className="mb-12 pb-2 border-b-2 border-solid border-b-gray-500">
-                <div className="flex">
-                    <div className="mt-4 mr-4">
-                        <Dropdown
-                            values={["Ej barn / ungdom", "Ej tillåten", "Fel säsong", "Felmärkt", "Fläckigt", "Nopprigt", "Omärkt", "Slitet", "Trasig", "Nyast", "Äldsta", "Säljare"]}
-                            default={defaults.sort.label}
-                            onSelect={value => {
-                                if(value === "Nyast") setSort("date");
-                                else if(value === "Äldsta") setSort("oldest");
-                                else if(value === "Säljare") setSort("seller");
-                                else setSort(value as DBSearch);
-                            }}
-                        />
-                    </div>
-                    <NumberField
-                        label="Antal plagg"
-                        onChange={setShow}
-                        values={{ default: defaults.show, min: 1 }}
-                        size="medium"
-                    />
-
-                    <div className="ml-14 h-fit mt-8">
-                        <Button variant="outlined" color="success" size="medium" className="h-10" onClick={e => {
-                            e.preventDefault();
-                            apply();
-                        }}>
-                            Tillämpa
-                        </Button>
-                    </div>
-                </div>
-                {sort === "seller" && <div className="flex ml-2">
-                    <NumberField
-                        label="Säljare"
-                        onChange={value => setSeller({ value, use: value })}
-                        values={{ default: defaults.sort.seller, min: 1 }}
-                        size="medium"
-                    />
-                    <FormGroup className="mt-8 ml-4">
-                        <FormControlLabel control={<Checkbox checked={seller.use === "*"} />} label="Alla" onClick={e => {
-                            e.preventDefault();
-                            setSeller({
-                                value: seller.value,
-                                use: seller.use === "*" ? seller.value : "*"
-                            });
-                        }} />
-                    </FormGroup>
-                </div>}
-            </Box>
-
-
-            <ItemDisplay items={items} />
+            <ItemDisplay items={items} slots={{
+                top: <ItemDisplay.TopControls defaults={defaults} onChange={apply}/>
+            }} />
         </Box>
     );
 }
--- components/ui/items/ItemDisplay.tsx
+++ components/ui/items/ItemDisplay.tsx
@@ -2,6 +2,7 @@ import { getImage } from "@barnloppis-se/api";
 import { ItemObject } from "@barnloppis-se/types/dist/src/data/item";
 import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import { ReactNode } from "react";
+import ItemDisplayTopControls from "./ItemDisplayTopControls";
 
 /**
  * Display props
@@ -11,6 +12,19 @@ interface Props {
      * List of items to display
      */
     readonly items?: ItemObject[]
+
+    /**
+     * Display slots
+     */
+    readonly slots?: {
+        /**
+         * Top slot nodes
+         *
+         * This will be placed over the
+         * display area.
+         */
+        readonly top?: ReactNode
+    }
 }
 
 
@@ -20,33 +34,44 @@ interface Props {
  */
 export default function ItemDisplay(props: Props): ReactNode {
     return(
-        <Grid container columns={{ sm: 3, md: 3, lg: 4 }} columnSpacing={4} rowSpacing={3} className="mb-12">
-            {props.items?.map(item => <Grid key={item.id} size={1}>
-                <Card className="max-w-96 min-w-56">
-                    <CardActionArea>
-                        <CardMedia
-                            className="h-60"
-                            component="img"
-                            src={getImage(item.id, 0)}
-                            alt={`Bild på ${item.tag} plagg`}
-                            loading="lazy"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" sx={{ letterSpacing: ".1rem" }} className="font-mono font-semibold">
-                                {item.tag}
-                            </Typography>
-                            <Box className="ml-3">
-                                {item.seller && <Typography variant="body2" color="textSecondary">
-                                    Säljare, <span>{item.seller}</span>
-                                </Typography>}
-                                <Typography variant="subtitle2" color="textSecondary" className="text-xs">
-                                    {item.id}
+        <Box>
+            {props.slots?.top}
+
+            <Grid container columns={{ sm: 3, md: 3, lg: 4 }} columnSpacing={4} rowSpacing={3} className="mb-12">
+                {props.items?.map(item => <Grid key={item.id} size={1}>
+                    <Card className="max-w-96 min-w-56">
+                        <CardActionArea>
+                            <CardMedia
+                                className="h-60"
+                                component="img"
+                                src={getImage(item.id, 0)}
+                                alt={`Bild på ${item.tag} plagg`}
+                                loading="lazy"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div" sx={{ letterSpacing: ".1rem" }} className="font-mono font-semibold">
+                                    {item.tag}
                                 </Typography>
-                            </Box>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Grid>)}
-        </Grid>
+                                <Box className="ml-3">
+                                    {item.seller && <Typography variant="body2" color="textSecondary">
+                                        Säljare, <span>{item.seller}</span>
+                                    </Typography>}
+                                    <Typography variant="subtitle2" color="textSecondary" className="text-xs">
+                                        {item.id}
+                                    </Typography>
+                                </Box>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Grid>)}
+            </Grid>
+        </Box>
     );
 }
+
+
+
+/**
+ * Item display top controls
+ */
+ItemDisplay.TopControls = ItemDisplayTopControls;
