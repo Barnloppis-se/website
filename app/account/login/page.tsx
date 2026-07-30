27ab37530b4fe78e4537deb3d38d256dba95a4c7
Xtarii <carlalvinelias@gmail.com>
2026-07-30 21:27:21 +0200

    account login page

--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 import { useNavigator } from "@/components/app/Navigator";
 import { useAuth } from "@/components/auth/auth";
+import LoadingIndicator from "@/components/ui/popup/LoadingIndicator";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import Image from "next/image";
 import { ReactNode, useState } from "react";
 
 /**
@@ -15,44 +17,66 @@ export default function Login(): ReactNode {
     const [ password, setPassword ] = useState("");
 
     const [ error, setError ] = useState<string>();
+    const [ load, setLoad ] = useState(false);
 
     return(
         <Box className="flex w-full h-fit py-10">
-            <form className="m-auto" onSubmit={e => {
+            <form className="m-auto bg-blue-50 px-8 py-12 rounded-sm" onSubmit={e => {
                 e.preventDefault();
+                setLoad(true);
                 auth.login({ email, password }).then(_ => {
                     if(auth.user) navigator.navigate("/");
                     else setError("Kunde inte logga in");
+                    setLoad(false);
                 })
             }}>
-                <TextField
-                    id="email"
-                    label="Email"
-                    type="email"
-                    required
-                    placeholder="Fyll i email"
-                    value={email}
-                    onChange={e => {
-                        e.preventDefault();
-                        setEmail(e.target.value);
-                    }}
-                />
-                <TextField
-                    className="my-3"
-                    id="password"
-                    label="Lösenord"
-                    type="password"
-                    required
-                    placeholder="Fyll i lösenord"
-                    value={password}
-                    onChange={e => {
-                        e.preventDefault();
-                        setPassword(e.target.value);
-                    }}
-                />
-                <Button type="submit" variant="outlined">Logga in</Button>
-                <Typography color="error">{error}</Typography>
+                <div className="pb-10">
+                    <Image
+                        className="w-80 h-auto rounded-sm"
+                        src={"/Logo - Barnloppis ( stor ).jpeg"}
+                        alt="Barnloppis.se loga (stor)"
+                        width={400}
+                        height={400}
+                        loading="lazy"
+                    />
+                </div>
+                <Box className="w-full flex flex-col">
+                    <div className="m-auto">
+                        <TextField
+                            id="email"
+                            label="Email"
+                            type="email"
+                            required
+                            placeholder="Fyll i email"
+                            value={email}
+                            onChange={e => {
+                                e.preventDefault();
+                                setEmail(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="m-auto">
+                        <TextField
+                            className="my-3"
+                            id="password"
+                            label="Lösenord"
+                            type="password"
+                            required
+                            placeholder="Fyll i lösenord"
+                            value={password}
+                            onChange={e => {
+                                e.preventDefault();
+                                setPassword(e.target.value);
+                            }}
+                        />
+                    </div>
+                </Box>
+                <Box className="w-full flex">
+                    <Button className="m-auto mt-5" type="submit" variant="outlined">Logga in</Button>
+                </Box>
+                <Typography className="ml-5 mt-5" color="error">{error}</Typography>
             </form>
+            {load && <LoadingIndicator label="Loggar in..." />}
         </Box>
     );
 }
